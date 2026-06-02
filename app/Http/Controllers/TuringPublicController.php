6b7539c762576998be094dac9c41c5d60142508d
andrea-bartiromo <bart3052@gmail.com>
2026-06-02 08:24:42 +0200

    Add public Turing index action

--- app/Http/Controllers/TuringPublicController.php
+++ app/Http/Controllers/TuringPublicController.php
@@ -6,6 +6,22 @@ use Illuminate\View\View;
 
 class TuringPublicController extends Controller
 {
+    public function index(): View
+    {
+        return view('turing.index', [
+            'page' => null,
+            'content' => [],
+            'hero' => [],
+            'intro' => [],
+            'editorialBlocks' => collect([]),
+            'why' => [],
+            'timeline' => collect([]),
+            'final' => [],
+            'sectionImageFallbacks' => [],
+            'sectionBackgroundFallbacks' => [],
+        ]);
+    }
+
     public function macchinaUniversale(): View
     {
         return view('turing.longform.macchina-universale');
