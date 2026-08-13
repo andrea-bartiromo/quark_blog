1f1b995c7ef7a2e7c49bb49f855ffec30faae473
Andrea Bartiromo <bart3052@gmail.com>
2026-08-13 11:55:55 +0200

    feat(content): expose non-personal path analytics metadata

--- resources/views/articles/partials/path-continuation.blade.php
+++ resources/views/articles/partials/path-continuation.blade.php
@@ -3,6 +3,7 @@
     class="path-continuation"
     aria-labelledby="path-continuation-title"
     data-path-slug="{{ $pathNavigation['cluster']->slug }}"
+    data-article-id="{{ $article->id }}"
     data-path-position="{{ $pathNavigation['current_index'] }}"
     data-path-total="{{ $pathNavigation['total'] }}"
 >
@@ -10,7 +11,7 @@
         <div>
             <p class="path-continuation__eyebrow">Percorso</p>
             <h2 id="path-continuation-title">Continua il percorso</h2>
-            <a class="path-continuation__name" href="{{ $pathNavigation['path_url'] }}">
+            <a class="path-continuation__name" href="{{ $pathNavigation['path_url'] }}" data-path-event="path_view_all_click">
                 {{ $pathNavigation['cluster']->name }}
             </a>
         </div>
@@ -22,14 +23,14 @@
     @if($pathNavigation['previous'] || $pathNavigation['next'])
         <nav class="path-continuation__nav" aria-label="Navigazione nel percorso {{ $pathNavigation['cluster']->name }}">
             @if($pathNavigation['previous'])
-                <a class="path-continuation__step path-continuation__step--previous" href="{{ route('articolo', $pathNavigation['previous']->slug) }}">
+                <a class="path-continuation__step path-continuation__step--previous" href="{{ route('articolo', $pathNavigation['previous']->slug) }}" data-path-event="path_previous_click">
                     <span class="path-continuation__direction">Precedente</span>
                     <span>{{ $pathNavigation['previous']->title }}</span>
                 </a>
             @endif
 
             @if($pathNavigation['next'])
-                <a class="path-continuation__step path-continuation__step--next" href="{{ route('articolo', $pathNavigation['next']->slug) }}">
+                <a class="path-continuation__step path-continuation__step--next" href="{{ route('articolo', $pathNavigation['next']->slug) }}" data-path-event="path_next_click">
                     <span class="path-continuation__direction">Successivo</span>
                     <span>{{ $pathNavigation['next']->title }}</span>
                 </a>
@@ -37,6 +38,6 @@
         </nav>
     @endif
 
-    <a class="path-continuation__all" href="{{ $pathNavigation['path_url'] }}">Vedi tutto il percorso</a>
+    <a class="path-continuation__all" href="{{ $pathNavigation['path_url'] }}" data-path-event="path_view_all_click">Vedi tutto il percorso</a>
 </section>
 @endif
