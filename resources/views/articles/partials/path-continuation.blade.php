09f99939d0b2008c30ec529cb5e67d9009234af1
Andrea Bartiromo <bart3052@gmail.com>
2026-08-15 11:19:22 +0200

    fix(percorsi): preserve existing continuation CTA contract

--- resources/views/articles/partials/path-continuation.blade.php
+++ resources/views/articles/partials/path-continuation.blade.php
@@ -23,6 +23,6 @@
             <span>Il prossimo capitolo arriverà qui.</span>
         </aside>
     @endif
-    <div class="path-continuation__footer"><a class="path-continuation__all" href="{{ $pathNavigation['path_url'] }}" data-path-event="path_view_all_click">Torna al Percorso “{{ $pathNavigation['cluster']->name }}” <span aria-hidden="true">→</span></a></div>
+    <div class="path-continuation__footer"><a class="path-continuation__all" href="{{ $pathNavigation['path_url'] }}" data-path-event="path_view_all_click">Vedi tutto il percorso “{{ $pathNavigation['cluster']->name }}” <span aria-hidden="true">→</span></a></div>
 </section>
 @endif
