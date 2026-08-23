529a5d2264053b34c35652c088b2146cc9f351ba
Andrea Bartiromo <bart3052@gmail.com>
2026-08-23 10:03:02 +0200

    fix(revisions): hide collaborator restore for editor-only historical states

--- resources/views/redazione/article-revisions/show.blade.php
+++ resources/views/redazione/article-revisions/show.blade.php
@@ -2,11 +2,21 @@
 @section('title','Versione del '.$revision->created_at->timezone('Europe/Rome')->format('d/m/Y H:i'))
 @section('content')
 
+@php
+  $revisionHasEditorOnlyStatus = in_array($revision->status, [
+      \App\Models\Article::STATUS_SCHEDULED,
+      \App\Models\Article::STATUS_PUBLISHED,
+  ], true);
+  $articleIsPublished = $article->status === \App\Models\Article::STATUS_PUBLISHED;
+@endphp
+
 @include('partials.article-revision-comparison', [
   'indexUrlName' => 'redazione.articles.revisions.index',
   'restoreUrl' => route('redazione.articles.revisions.restore', [$article, $revision]),
-  'canRestore' => $article->status !== 'published',
-  'cannotRestoreReason' => 'Gli articoli pubblicati non possono essere modificati. Contatta l\'editor.',
+  'canRestore' => ! $articleIsPublished && ! $revisionHasEditorOnlyStatus,
+  'cannotRestoreReason' => $articleIsPublished
+      ? 'Gli articoli pubblicati non possono essere modificati. Contatta l\'editor.'
+      : 'Questa versione aveva uno stato riservato all\'editor e non può essere ripristinata dalla Redazione.',
 ])
 
 @endsection
