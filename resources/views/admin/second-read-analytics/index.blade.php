6d3df3ae157ec1488beebc4efc06c51a0891e218
Andrea Bartiromo <bart3052@gmail.com>
2026-08-27 19:09:43 +0200

    fix(second-read): use aggregate cluster key

--- resources/views/admin/second-read-analytics/index.blade.php
+++ resources/views/admin/second-read-analytics/index.blade.php
@@ -68,7 +68,7 @@
         <tbody>
           @foreach($pathBreakdown as $row)
             <tr>
-              <td><a href="{{ route('admin.content-clusters.edit', $row['id']) }}">{{ $row['name'] }}</a></td>
+              <td><a href="{{ route('admin.content-clusters.edit', $row['content_cluster_id']) }}">{{ $row['name'] }}</a></td>
               <td>{{ number_format($row['impressions']) }}</td>
               <td>{{ number_format($row['second_reads']) }}</td>
               <td>{{ number_format($row['second_read_rate'] * 100, 1) }}%</td>
