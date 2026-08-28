ebf544059d4a7267c82780fce76d07d812366210
Andrea Bartiromo <bart3052@gmail.com>
2026-08-28 10:33:55 +0200

    style(test): satisfy Pint class separation

--- tests/Feature/FooterSocialLinksTest.php
+++ tests/Feature/FooterSocialLinksTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 class FooterSocialLinksTest extends TestCase
 {
     use RefreshDatabase;
+
     private const LINKEDIN = 'https://www.linkedin.com/company/kairus-it/';
 
     private const FACEBOOK = 'https://www.facebook.com/profile.php?id=61593323495927';
